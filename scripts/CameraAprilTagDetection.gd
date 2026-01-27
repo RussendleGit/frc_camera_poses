c65ed1b401c73e9ae328d55c6c76750c3b06f2ad
Russell Jarvis <russelljarvis2008@gmail.com>
2026-01-27 12:49:14 -0700

    refactored for filtering

--- scripts/CameraAprilTagDetection.gd
+++ scripts/CameraAprilTagDetection.gd
@@ -1,15 +1,11 @@
 extends Node3D
 
-@export var camera_fov_degrees: float = 100.0
-@export var max_distance: float = 200.0 / 39.37
-@export var max_tag_angle_to_cam: float = 80
 
-@export var num_poses_grid: Vector2 = Vector2(40.0, 40.0)
+
+@export var num_poses_grid: Vector2 = Vector2(5.0, 5.0)
 @export var rotation_increment_degrees: float = 5
 @export var camera_translation_increment: float = 0.1
 @export var field_dimensions_meters: Vector2 = Vector2(16.540988, 8.069326)
-
-
 
 @onready var tag_directory: Node3D = $TagDirectory
 @onready var camera_directory: Node3D = $CameraDirectory
@@ -26,7 +22,7 @@ func _ready() -> void:
 	position_translation_increment = Vector2(field_dimensions_meters.x / num_poses_grid.x, field_dimensions_meters.y / num_poses_grid.y)
 	tag_directory.set_april_tags()
 
-	camera_directory.add_camera(Vector3(0.0, -0.2, 0.4), Vector3(0.0, 15.0, 30.0)) 
+	#camera_directory.add_camera(Vector3(0.0, -0.2, 0.4), Vector3(0.0, 15.0, 30.0)) 
 	camera_directory.add_camera(Vector3(0.0, 0.2, 0.4), Vector3(0.0, 15.0, -30.0)) 
 	camera_directory.setup_cameras()
 
@@ -34,106 +30,22 @@ func _ready() -> void:
 
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if robot_collision.has_overlapping_bodies():
-		print("rejected")
 		move_robot()
 		tag_directory.update_raycasts_for_next_iteration(camera_directory.get_current_camera())
 		camera_directory.skip_current_iteration()
 		return
 		
-	var unblocked_tags = filter_tags_by_raycast()
-	var tags_within_view_angle = filter_tags_by_cam_view_angle(camera_directory.get_current_camera(), unblocked_tags)
-	var tags_within_distance = filter_tags_by_distance(camera_directory.get_current_camera(), tags_within_view_angle)	
-	var tags_within_tag_angle = filter_tags_by_tag_angle(camera_directory.get_current_camera(), tags_within_distance)
+	var unblocked_tags = camera_directory.filter_april_tags(tag_directory.get_all_tags())
 
+	# determine if all the cameras have been processed. If so, test for the next pose
 	var is_reset = camera_directory.next_camera()
 	if is_reset:
 		move_robot()
 
 	tag_directory.update_raycasts_for_next_iteration(camera_directory.get_current_camera())
-	
-	
-## gives a list of the tags that are not blocked by anything
-## doesn't need camera attribute as the raycasts have been already handled by update_raycasts_for_next_iteration
-func filter_tags_by_raycast() -> Array[Node3D]:
-	var unblocked_april_tags: Array[Node3D] = []
-	
-	# get every tag in the tag directory in the camera
-	for tag in tag_directory.get_all_tags():
-		# for each corner of the april tag scene
-		var see_all_four_corners = true
-		for ray_cast in tag.get_ray_casts():
-			if ray_cast.is_colliding():
-				see_all_four_corners = false
-			
-		if see_all_four_corners:
-			unblocked_april_tags.append(tag)
-			tag.visible = true
-		else:
-			tag.visible = false
-	
-	return unblocked_april_tags
 
-## get the tags that are within view angles
-func filter_tags_by_cam_view_angle(camera_attribute: Node3D, tags: Array[Node3D]) -> Array[Node3D]:
-	# horizontal
-	var tags_in_view: Array[Node3D] = []
-	for tag in tags:
-		var direction = tag.global_position - camera_attribute.global_position
-		
-		# get 2d values for x, and z
-		var direction_2d_yaw = Vector2(direction.x, direction.z)
-		var direction_2d_pitch = Vector2(direction.x, direction.y)
-		
-		# get the angle between the tags
-		var angle_yaw = direction_2d_yaw.angle()
-		var angle_pitch = direction_2d_pitch.angle()
-
-		# account rotation of marker
-		var angle_diff_yaw = wrapf(angle_yaw + camera_attribute.global_rotation.y, -PI, PI)
-		var angle_diff_pitch_left = wrapf(angle_pitch + camera_attribute.global_rotation.z, -PI, PI)
-		var angle_diff_pitch_right = wrapf(angle_pitch + camera_attribute.global_rotation.z - PI, -PI, PI)
-
-		if abs(angle_diff_yaw) <= deg_to_rad(camera_fov_degrees / 2.0) && \
-			(abs(angle_diff_pitch_left) <= deg_to_rad(camera_fov_degrees / 2.0) || \
-			abs(angle_diff_pitch_right) <= deg_to_rad(camera_fov_degrees / 2.0)):
-			tag.visible = true
-			tags_in_view.append(tag)
-		else:
-			tag.visible = false
-	
-	return tags_in_view
-
-func filter_tags_by_distance(camera_attribute: Node3D, tags: Array[Node3D]) -> Array[Node3D]:
-	var tags_within_range: Array[Node3D] = []
-	
-	for tag in tags:
-		if camera_attribute.global_position.distance_to(tag.global_position) < max_distance:
-			tag.visible = true
-			tags_within_range.append(tag)
-		else:
-			tag.visible = false
-			 
-	return tags_within_range		
-
-func filter_tags_by_tag_angle(camera_attribute: Node3D, tags: Array[Node3D]) -> Array[Node3D]:
-	var tags_within_angle: Array[Node3D] = []
-	
-	for tag in tags:
-		# take away 90 from yaw, because that's the default rotation for april tags
-		var yaw = wrapf((tag.global_rotation.y - deg_to_rad(90.0)) - camera_attribute.global_rotation.y, -PI, PI)
-		var pitch = wrapf(tag.global_rotation.z - camera_attribute.global_rotation.z, -PI, PI)
-		tag.skew_yaw = yaw
-		tag.skew_pitch = pitch
-
-		if abs(yaw) <= deg_to_rad(max_tag_angle_to_cam) && abs(pitch) <= deg_to_rad(max_tag_angle_to_cam):
-			tag.visible = true
-			tags_within_angle.append(tag)
-		else:
-			tag.visible = false 
-		
-	return tags_within_angle
 
 func move_robot():
 	var new_rot: float = camera_directory.global_rotation_degrees.y + rotation_increment_degrees
--- scripts/cameras.gd
+++ scripts/cameras.gd
@@ -1,12 +1,18 @@
 extends Node3D
 
+@export var max_tag_distance: float = 200.0 / 39.37
+@export var max_tag_skew_degrees: float = 80
+@export var camera_fov_degrees: float = 100.0
+
 var camera_attributes: Array[Node3D] = []
 var current_camera_used_index: int = 0
+var current_camera: Node3D
 
 func setup_cameras():
 	for i in range(len(camera_attributes)):
 		camera_attributes[i].name = str(i)
 		add_child(camera_attributes[i])
+	current_camera = camera_attributes[0]
 
 ## adds a camera, also flips y and z for ease of use
 func add_camera(cam_position_meters: Vector3, cam_rotation_degrees: Vector3):
@@ -23,10 +29,67 @@ func get_current_camera():
 ## returns a bool weather it has gone back to the first camera
 func next_camera() -> bool:
 	current_camera_used_index += 1
-	if (current_camera_used_index >= len(camera_attributes)):
+	var is_reset: bool = current_camera_used_index >= len(camera_attributes)
+	if (is_reset):
 		current_camera_used_index = 0
-		return true
-	return false
+	current_camera = camera_attributes[current_camera_used_index]
+	return is_reset
 
 func skip_current_iteration():
 	current_camera_used_index = 0
+
+func filter_april_tags(tags: Array[Node]) -> Array[Node3D]:
+	var viewable_tags: Array[Node3D] = []
+	
+	# get every tag in the tag directory in the camera
+	for tag in tags:
+		# ignore of which the raycast is colliding
+		var see_all_four_corners = true
+		for ray_cast in tag.get_ray_casts():
+			if ray_cast.is_colliding():
+				see_all_four_corners = false
+				break
+		
+		if !see_all_four_corners:
+			tag.visible = false
+			continue
+
+		# ignore tags that are too far away
+		if current_camera.global_position.distance_to(tag.global_position) > max_tag_distance:
+			continue
+
+		# ignore tags that are too skewed
+		var skew_yaw = wrapf((tag.global_rotation.y - deg_to_rad(90.0)) - current_camera.global_rotation.y, -PI, PI)
+		var skew_pitch = wrapf(tag.global_rotation.z - current_camera.global_rotation.z, -PI, PI)
+		tag.skew_yaw = skew_yaw
+		tag.skew_pitch = skew_pitch
+		if abs(skew_yaw) > deg_to_rad(max_tag_skew_degrees) || abs(skew_pitch) > deg_to_rad(max_tag_skew_degrees):
+			tag.visible = false
+			continue
+
+		# for ignoring tags outside of fov
+		var direction = tag.global_position - current_camera.global_position
+		
+		# ignore tags outside fov yaw
+		var direction_2d_yaw = Vector2(direction.x, direction.z)
+		var angle_yaw = direction_2d_yaw.angle()
+		var angle_diff_yaw = wrapf(angle_yaw + current_camera.global_rotation.y, -PI, PI)
+		if abs(angle_diff_yaw) > deg_to_rad(camera_fov_degrees / 2.0):
+			tag.visible = false
+			continue
+
+		# ignore tags outside of pitch
+		var direction_2d_pitch = Vector2(direction.x, direction.y)
+		var angle_pitch = direction_2d_pitch.angle()
+		var angle_diff_pitch_left = wrapf(angle_pitch + current_camera.global_rotation.z, -PI, PI)
+		var angle_diff_pitch_right = wrapf(angle_pitch + current_camera.global_rotation.z - PI, -PI, PI)
+		if (abs(angle_diff_pitch_left) > deg_to_rad(camera_fov_degrees / 2.0) &&
+			abs(angle_diff_pitch_right) > deg_to_rad(camera_fov_degrees / 2.0)):
+			tag.visible = false
+			continue
+
+
+		
+		viewable_tags.append(tag)
+		tag.visible = true
+	return viewable_tags
